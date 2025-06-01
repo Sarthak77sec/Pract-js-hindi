1b3d03e64d32278b32712d0d9859f26644768827
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-01 13:37:09 +0000

    done with convesion today

---
+++ Practice-01/conv.js
@@ -0,0 +1,8 @@
+let score="321"
+console.log(typeof(score));
+
+let valueNumber = Number(score)
+console.log(valueNumber);
+console.log(typeof(valueNumber));
+
+// this is how conversion is done in js
