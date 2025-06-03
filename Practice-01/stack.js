b5d9b570549d216d484983b83b861ff8e5dee0b1
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-03 12:14:29 +0000

    done with math

---
+++ Practice-01/stack.js
@@ -0,0 +1,26 @@
+
+let userOne = {
+    email:"tim@gmai.com",
+    upi_id:"ybl@5567",
+    name:"tim"
+}
+
+let userTwo = {
+    email:"trevor@gmai.com",
+    upi_id:"ybss@555",
+    name:"trevor"
+}
+
+// console.log(userTwo);
+// console.log(userOne)
+
+let name="rajan"
+let count=50
+
+// console.log(`hello my name is ${name} and my ${userOne.upi_id.toLocaleUpperCase()} body count is ${count}`)
+// numbers and maths in java script
+const score = 100
+console.log(score);
+
+const data = new Number(300)
+console.log(data);
