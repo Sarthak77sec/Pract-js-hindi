0a7098e60e4bd5a5fbf238c5fd5c660bdf47f527
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-07 16:06:08 +0000

    "arrays basic function"

--- Practice-01/ars.js
+++ Practice-01/ars.js
@@ -2,5 +2,9 @@ const marvel_heroes=["thor","ironman","spiderman"]
 
 const dc =["superman","batman","flash"]
 
-const new_heroes=[...marvel_heroes,...dc]
-console.log(new_heroes)
+// const new_heroes=[...marvel_heroes,...dc]
+// console.log(new_heroes)
+
+const name ="sarthak"
+
+console.log(Array.from(name))
