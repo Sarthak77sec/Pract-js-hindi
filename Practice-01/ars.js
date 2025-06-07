6a271213b8831b3e78fab42bad7f64ad09dedf68
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-07 16:14:10 +0000

    arrays basics

--- Practice-01/ars.js
+++ Practice-01/ars.js
@@ -5,6 +5,8 @@ const dc =["superman","batman","flash"]
 // const new_heroes=[...marvel_heroes,...dc]
 // console.log(new_heroes)
 
-const name ="sarthak"
+let score1 =100
+let score2 =200
+let score3 =300
 
-console.log(Array.from(name))
+console.log(Array.of(score1,score2,score3));
