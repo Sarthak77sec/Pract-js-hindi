2c9d5871d1856679b01bcfa39db590c00f34dd09
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-09 17:14:00 +0000

    more with objects

--- Practice-01/objects.js
+++ Practice-01/objects.js
@@ -6,4 +6,31 @@ const js_user={
     logindays:["Monday","tuesday","wednesday"]
 
 }
-console.log(js_user.email);
+// console.log(js_user.email);
+
+const tinder ={}
+tinder.id="123abc"
+tinder.name="sonic"
+tinder.loggedin=false
+
+// console.log(tinder);
+// nestedobjects 
+const regularuser={
+    email:"mink@gmail.com",
+    full_name:{
+        userfull_name:{
+            firstname:"hitesh",
+            lastname:"choudhary"
+        }
+
+    }
+
+}
+// console.log(regularuser.full_name.userfull_name.firstname);
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+// const obj3=Object.assign({},obj1,obj2)
+//using spread opertor
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
