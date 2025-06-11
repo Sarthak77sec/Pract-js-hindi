0ac79d0f9bfead920b52f47ec72df10ec605e919
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-11 18:03:56 +0000

    function with objects and arrays

--- Practice-01/funcs.js
+++ Practice-01/funcs.js
@@ -6,4 +6,22 @@ else{
      return `${username} just logged in`
    }
  }
-console.log(greet());
+// console.log(greet());
+
+//function with array and objects
+const item={
+    item_name:"trimmer",
+    price:899
+}
+
+function objecthandler(anyobject){
+//    console.log(`the item name is ${anyobject.item_name} and price is ${anyobject.price}`);
+}
+objecthandler(item)
+
+// function with array
+const Array1 =[300,400,500,200]
+function myarray(getArray){
+    return getArray[2]
+}
+console.log(myarray(Array1))
