7c06a15eb954a4603f8f97b7d2791c818d9124b5
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-16 17:37:42 +0000

    what are iife function

---
+++ Practice-01/iife.js
@@ -0,0 +1,11 @@
+// immidiately invoked function
+// named iife function
+(function chai(user){
+    console.log(`${user},DB CONNECTED`);
+    
+})("blackbird");
+//unnamed iife
+((name)=>{
+    console.log(`${name},db connected`);
+    
+})("robin")
