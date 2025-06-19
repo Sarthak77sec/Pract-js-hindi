8c3f8e48cf2790e557f996883198ee7c33a9d795
Sarthak77sec <sarthak6789sharma@gmail.com>
2025-06-19 16:52:45 +0000

    loops in js

--- Practice-01/loops.js
+++ Practice-01/loops.js
@@ -1,8 +1,20 @@
-for(i=0;i<10;i++){
-    console.log(i);
-    if(i==5){
-        console.log("we are at five");
-break
-    }
+// for(i=0;i<10;i++){
+//     console.log(i);
+//     if(i==5){
+//         console.log("we are at five");
+// break
+//     }
     
+// }
+const myarray =["chip","dave","rob","mike"]
+i=0
+while (i<myarray.length) {
+    //console.log(myarray[i]);
+i+=1    
 }
+//do while loop
+j=0
+do{
+console.log(j);
+j+=1
+}while(j<=10)
